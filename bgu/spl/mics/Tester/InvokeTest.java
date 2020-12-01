733645cf23aa38f250858dd8730173737eceb164
RRIL97 <62291019+RRIL97@users.noreply.github.com>
2020-12-01 14:52:06 +0330

    Create InvokeTest.java

---
+++ bgu/spl/mics/Tester/InvokeTest.java
@@ -0,0 +1,21 @@
+package bgu.spl.mics.Tester;
+
+import java.util.Scanner;
+
+//Coded By Ron Rachev
+public class InvokeTest {
+
+    public static void main(String [] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type 2 to generate tests into the tests.json file");
+        System.out.println("Type 1 to run the tests from the tests.json file");
+        int choosenOption = in.nextInt();
+        Tester myTester = new Tester();
+
+        if(choosenOption == 2)
+            myTester.generateTests();
+        else
+            myTester.runTests();
+    }
+}
