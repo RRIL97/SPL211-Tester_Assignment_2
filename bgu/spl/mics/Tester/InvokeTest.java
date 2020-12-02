255525f25c1e5955fc7c889297d92e28b3b8097e
RRIL97 <62291019+RRIL97@users.noreply.github.com>
2020-12-02 21:33:43 +0330

    Update InvokeTest.java

--- bgu/spl/mics/Tester/InvokeTest.java
+++ bgu/spl/mics/Tester/InvokeTest.java
@@ -8,14 +8,16 @@ public class InvokeTest {
     public static void main(String [] args)
     {
         Scanner in = new Scanner(System.in);
+        System.out.println("Type 3 to run logical tests");
         System.out.println("Type 2 to generate tests into the tests.json file");
         System.out.println("Type 1 to run the tests from the tests.json file");
         int choosenOption = in.nextInt();
         Tester myTester = new Tester();
 
-        if(choosenOption == 2)
-            myTester.generateTests();
-        else
-            myTester.runTests();
+        if(choosenOption == 2)  myTester.generateTests();  else
+            if(choosenOption == 1)
+            myTester.runTestsFromFile();
+            else if(choosenOption == 3)
+            myTester.runLogicalTests();
     }
 }
